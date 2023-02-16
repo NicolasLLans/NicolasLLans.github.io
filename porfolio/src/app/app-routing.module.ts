40b36e7eb85317fa9b2694ae6e139bd874399a45
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-02-16 18:25:28 -0300

    mejora en el diseño de la estructura y diseño de barra de navegación

--- porfolio/src/app/app-routing.module.ts
+++ porfolio/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:ProyectosComponent},
+  {path: 'sobre-mi', component:AcercaDeComponent},
+  {path: 'Proyectos', component:ProyectosComponent},
+  {path: '**', component:ProyectosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- porfolio/src/app/app.module.ts
+++ porfolio/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarranavComponent } from './componentes/barranav/barranav.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarranavComponent
+    BarranavComponent,
+    ProyectosComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
