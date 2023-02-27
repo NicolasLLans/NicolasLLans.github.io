e58709435232d09ad8380705a2ada4641c264178
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-02-27 18:49:32 -0300

    mejora en las rutas

--- porfolio/src/app/app-routing.module.ts
+++ porfolio/src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { PanelComponent } from './componentes/panel/panel.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 const routes: Routes = [
-  {path: '', component:ProyectosComponent},
+  {path: '', component:PanelComponent},
   {path: 'sobre-mi', component:AcercaDeComponent},
   {path: 'proyectos', component:ProyectosComponent},
   {path: 'panel', component:PanelComponent},
-  {path: '**', component:ProyectosComponent},
+  {path: '**', component:PanelComponent},
 ];
 
 @NgModule({
